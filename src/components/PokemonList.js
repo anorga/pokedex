bad5679a68faeaaf0222401012b25bd2f68d8cd2
Christian Anorga <anorga2990@gmail.com>
2022-10-13 15:29:47 -0700

    update

--- src/components/PokemonList.js
+++ src/components/PokemonList.js
@@ -2,6 +2,7 @@ const PokemonList = ({ pokemon }) => {
   return (
     <>
       <div>
+        <h1>POKEMON POKEDEX</h1>
         {pokemon.map((p) => (
           <div key={p}>{p}</div>
         ))}
