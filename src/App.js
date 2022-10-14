012045adcc70c45de7a87756c082929aac19c482
Christian Anorga <anorga2990@gmail.com>
2022-10-14 00:20:54 -0700

    added pokeAPI

--- src/App.js
+++ src/App.js
@@ -2,14 +2,17 @@ import React, { useState } from "react";
 import "./App.css";
 import PokemonList from "./components/PokemonList";
 import Nav from "./components/Nav";
+import axios from "axios";
 
 function App() {
-  const [pokemon, setPokemon] = useState([
-    "charmander",
-    "pikachu",
-    "charizard",
-    "chansey",
-  ]);
+  const [pokemon, setPokemon] = useState([]);
+  const [pokemonURL, setPokemonURL] = useState(
+    "https://pokeapi.co/api/v2/pokemon/"
+  );
+
+  axios.get(pokemonURL).then((res) => {
+    setPokemon(res.data.results.map((p) => p.name));
+  });
 
   return (
     <>
