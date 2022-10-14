0389fa9c630e62e26217152238f80794c1db706b
Christian Anorga <anorga2990@gmail.com>
2022-10-14 12:38:50 -0700

    Update PokemonList.js

--- src/components/PokemonList.js
+++ src/components/PokemonList.js
@@ -13,9 +13,11 @@ function PokemonList({ pokemon }) {
           </tr>
         </thead>
         <tbody>
-            <tr>
-                
+          {pokemon.map((p) => (
+            <tr key={p}>
+              <td className="border-collapse border border-slate-500 px-4 py-2">{p}</td>
             </tr>
+          ))}
         </tbody>
       </table>
     </>
