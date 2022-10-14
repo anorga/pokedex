acb1a5cb64dc6166dc10be4fa11b7cd49ba0a91d
Christian Anorga <anorga2990@gmail.com>
2022-10-14 12:33:49 -0700

    added table

--- src/components/PokemonList.js
+++ src/components/PokemonList.js
@@ -1,11 +1,23 @@
 function PokemonList({ pokemon }) {
   return (
     <>
-      <div>
-        {pokemon.map((p) => (
-          <div key={Math.random()}>{p}</div>
-        ))}
-      </div>
+      <table className="border-collapse border border-slate-500">
+        <thead>
+          <tr>
+            <th className="border-collapse border border-slate-500 px-4 py-2">
+              Pokemon Name
+            </th>
+            <th className="border-collapse border border-slate-500 px-4 py-2">
+              Pokemon Sprite
+            </th>
+          </tr>
+        </thead>
+        <tbody>
+            <tr>
+                
+            </tr>
+        </tbody>
+      </table>
     </>
   );
 }
