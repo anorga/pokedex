cc10fde88ee150f4b9d5a1103e700de51e7610d2
Christian Anorga <anorga2990@gmail.com>
2022-10-16 14:27:20 -0700

    Update App.js
    cleaned up logic

--- src/App.js
+++ src/App.js
@@ -1,49 +1,13 @@
-import React, { useState, useEffect } from "react";
 import "./App.css";
-import PokemonList from "./components/PokemonList";
 import Nav from "./components/Nav";
-import Pagination from "./components/Pagination";
-import Sprites from "./components/Sprites";
-import axios from "axios";
+import Pokedex from "./Pokedex";
 
 function App() {
-  // POKEMON NAME STATE
-  const [pokemon, setPokemon] = useState([]);
-  const [currentPageUrl, setCurrentPageUrl] = useState(
-    "https://pokeapi.co/api/v2/pokemon/"
-  );
-  // PAGINATION STATE
-  const [nextPageUrl, setNextPageUrl] = useState();
-  const [prevPageUrl, setPrevPageUrl] = useState();
-
-  // SPRITES STATE
-  let [pokemonNumber, setPokemonNumber] = useState(0);
-
-  useEffect(() => {
-    axios.get(currentPageUrl).then((res) => {
-      setNextPageUrl(res.data.next);
-      setPrevPageUrl(res.data.previous);
-      setPokemon(res.data.results.map((p) => p.name));
-    });
-  }, [currentPageUrl, pokemonNumber]);
-
-  // PAGINATION FX
-  function nextPage() {
-    setCurrentPageUrl(nextPageUrl);
-    setPokemonNumber(+20);
-  }
-
-  function prevPage() {
-    setCurrentPageUrl(prevPageUrl);
-    setPokemonNumber(-20);
-  }
 
   return (
     <>
       <Nav />
-      <Sprites />
-      <PokemonList pokemon={pokemon} pokemonNumber={pokemonNumber} />
-      <Pagination nextPage={nextPage} prevPage={prevPage} />
+      <Pokedex />
     </>
   );
 }
