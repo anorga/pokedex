84d111253cf485fe2da9500b99195e019ed4718c
Christian Anorga <anorga2990@gmail.com>
2022-10-18 00:30:04 -0700

    Update Pagination.js

--- src/components/Pagination.js
+++ src/components/Pagination.js
@@ -1,25 +1,44 @@
-import { ChevronLeftIcon, ChevronRightIcon } from '@heroicons/react/20/solid'
-
-
+import { ChevronLeftIcon, ChevronRightIcon } from "@heroicons/react/20/solid";
 
 function Pagination({ prevPage, nextPage }) {
   return (
-    <div className="bg-sky-50 flex justify-center py-6">
-      <button
-        type="button"
-        className="inline-flex items-center rounded-md border border-gray-300 bg-white px-8 py-4 mr-1 text-sm font-medium text-gray-700 shadow-sm hover:bg-gray-50 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2"
-        onClick={prevPage}
-      >
-        Previous
-      </button>
-      <button
-        type="button"
-        className="inline-flex items-center rounded-md border border-gray-300 bg-white px-12 py-4 text-sm font-medium text-gray-700 shadow-sm hover:bg-gray-50 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2"
-        onClick={nextPage}
-      >
-        Next
-      </button>
-    </div>
+    <>
+      <div className="rounded-md shadow-sm bg-sky-50 flex justify-center py-6">
+        <button
+          type="button"
+          className="relative inline-flex items-center rounded-l-md border border-gray-300 bg-white px-4 py-4 text-sm font-medium text-gray-500 hover:bg-gray-50 focus:z-10 focus:border-indigo-500 focus:outline-none focus:ring-1 focus:ring-indigo-500"
+          onClick={prevPage}
+       >
+          <span className="sr-only">Previous</span>
+          <ChevronLeftIcon className="h-5 w-5" aria-hidden="true" />
+        </button>
+        <button
+          type="button"
+          className="relative -ml-px inline-flex items-center rounded-r-md border border-gray-300 bg-white px-4 py-4 text-sm font-medium text-gray-500 hover:bg-gray-50 focus:z-10 focus:border-indigo-500 focus:outline-none focus:ring-1 focus:ring-indigo-500"
+          onClick={nextPage}
+        >
+          <span className="sr-only">Next</span>
+          <ChevronRightIcon className="h-5 w-5" aria-hidden="true" />
+        </button>
+      </div>
+
+      {/* <div className="bg-sky-50 flex justify-center py-6">
+        <button
+          type="button"
+          className="inline-flex items-center rounded-md border border-gray-300 bg-white px-8 py-4 mr-1 text-sm font-medium text-gray-700 shadow-sm hover:bg-gray-50 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2"
+          onClick={prevPage}
+        >
+          Previous
+        </button>
+        <button
+          type="button"
+          className="inline-flex items-center rounded-md border border-gray-300 bg-white px-12 py-4 text-sm font-medium text-gray-700 shadow-sm hover:bg-gray-50 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2"
+          onClick={nextPage}
+        >
+          Next
+        </button>
+      </div> */}
+    </>
   );
 }
 
