8c0fb801c90459b62993ca4d09627e76aa9a893a
Christian Anorga <anorga2990@gmail.com>
2022-10-18 10:49:55 -0700

    Update Pagination.js

--- src/components/Pagination.js
+++ src/components/Pagination.js
@@ -21,23 +21,6 @@ function Pagination({ prevPage, nextPage }) {
           <ChevronRightIcon className="h-5 w-5" aria-hidden="true" />
         </button>
       </div>
-
-      {/* <div className="bg-sky-50 flex justify-center py-6">
-        <button
-          type="button"
-          className="inline-flex items-center rounded-md border border-gray-300 bg-white px-8 py-4 mr-1 text-sm font-medium text-gray-700 shadow-sm hover:bg-gray-50 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2"
-          onClick={prevPage}
-        >
-          Previous
-        </button>
-        <button
-          type="button"
-          className="inline-flex items-center rounded-md border border-gray-300 bg-white px-12 py-4 text-sm font-medium text-gray-700 shadow-sm hover:bg-gray-50 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2"
-          onClick={nextPage}
-        >
-          Next
-        </button>
-      </div> */}
     </>
   );
 }
