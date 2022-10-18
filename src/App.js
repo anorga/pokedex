89931e75e192efed0f6bfc6186138396f55addb9
Christian Anorga <anorga2990@gmail.com>
2022-10-18 10:11:12 -0700

    package updates

--- src/App.js
+++ src/App.js
@@ -1,16 +1,14 @@
-import "./App.css";
 import Nav from "./components/Nav";
 import Pokedex from "./components/Pokedex";
 import Pokemon from "./components/Pokemon";
 import { Routes, Route } from "react-router-dom";
-import Example from "./components/Example";
+
 
 
 function App() {
   return (
     <>
       <Nav />
-      <Example />
       <Routes>
         <Route index path="/" element={<Pokedex />} />
         <Route path=":pokemonId" element={<Pokemon />} />
