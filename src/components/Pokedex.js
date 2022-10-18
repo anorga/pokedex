40fa013d7894d566c4c1e5bfc9a73749a31e9c9d
Christian Anorga <anorga2990@gmail.com>
2022-10-18 10:07:07 -0700

    Update Pokedex.js

--- src/components/Pokedex.js
+++ src/components/Pokedex.js
@@ -43,7 +43,7 @@ function Pokedex() {
           <div className="bg-gray-50 h-full" />
         </div>
         <div className="relative mx-auto max-w-7xl">
-          <div className="grid max-w-lg gap-5 mx-auto lg:grid-cols-4 lg:max-w-none">
+          <div className="grid max-w-lg gap-5 mx-auto lg:grid-cols-5 lg:max-w-none">
             {pokemon.map((p) => {
               pokemonNumber = pokemonNumber + 1;
               return (
