fbfbceeef774180a6c5e9fc8ffbbe903f05453d7
Christian Anorga <anorga2990@gmail.com>
2024-08-18 22:03:59 -0700

    footer text to white

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -4,7 +4,7 @@ function Footer() {
     const currentYear = new Date().getFullYear();
 
     return (
-        <div className="bg-gray-900 py-5 text-center text-slate-400 font-semibold z-10 text-2xl">
+        <div className="bg-gray-900 py-5 text-center text-white font-semibold z-10 text-2xl">
             <p className="flex items-center justify-center">
                 <img src={pokeball} alt="Pokeball" className="w-4 h-4 mr-1" />
                 {currentYear} Christian Anorga
