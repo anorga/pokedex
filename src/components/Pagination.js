f38d4d2efbe01f462328117550ec04273e2daa54
Christian Anorga <anorga2990@gmail.com>
2024-08-18 20:02:19 -0700

    Update Pagination.js

--- src/components/Pagination.js
+++ src/components/Pagination.js
@@ -5,7 +5,7 @@ function Pagination({ toPrevPage, toNextPage }) {
     <>
       <div className="bg-black flex justify-center py-4">
         {/* Removed bottom buttons */}
-        {/* <button
+        <button
           type="button"
           className="relative inline-flex items-center rounded-2xl border border-gray-300 bg-slate-200 px-4 py-4 mx-5 text-sm font-medium text-gray-900 hover:bg-slate-300 focus:z-10 focus:border-slate-500 focus:outline-none focus:ring-1 focus:ring-slate-500"
           onClick={toPrevPage}
@@ -20,7 +20,7 @@ function Pagination({ toPrevPage, toNextPage }) {
         >
           <span className="sr-only">Next</span>
           <ChevronRightIcon className="h-5 w-5" aria-hidden="true" />
-        </button> */}
+        </button>
       </div>
     </>
   );
