ad11d18f0c4905cca80552c5ff321cd28a2820e4
Christian Anorga <anorga2990@gmail.com>
2024-08-18 21:09:37 -0700

    removed margin from title on nav

--- src/components/Nav.js
+++ src/components/Nav.js
@@ -47,7 +47,7 @@ export default function Nav() {
                     />
                   </Link>
                 </div>
-                <div className="hidden sm:ml-6 sm:block">
+                <div className="hidden sm:block">
                   <div className="flex space-x-4">
                     {navigation.map((item) => (
                       <Link
