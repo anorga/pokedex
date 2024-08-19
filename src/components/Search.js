89146e974f84d8a9ec3d07c9bee81d1434e0a1b6
Christian Anorga <anorga2990@gmail.com>
2024-08-18 23:03:08 -0700

    search changes

--- src/components/Search.js
+++ src/components/Search.js
@@ -1,4 +1,6 @@
+import {setIsFocused, isFocused} from 'react';
 import { ChevronLeftIcon, ChevronRightIcon } from "@heroicons/react/20/solid";
+import magnify from "./assets/magnifying_glass.png"
 
 function Search({ toNextPage, toPrevPage, handleSearchChange, currentPage }) {
   return (
@@ -6,55 +8,46 @@ function Search({ toNextPage, toPrevPage, handleSearchChange, currentPage }) {
       {/* Left Button */}
       <button
         type="button"
-        className={`rounded-2xl border border-gray-400 px-4 py-4 mx-5 text-sm font-medium text-gray-900 focus:z-10 focus:border-slate-500 focus:outline-none focus:ring-1 focus:ring-slate-500 ${
+        className={`px-4 py-4 mx-5 md:mx-20 ${
           currentPage === 1
-            ? "bg-slate-600 cursor-not-allowed"
-            : "bg-slate-400 hover:bg-slate-300"
+            ? "cursor-not-allowed"
+            : "bg-white"
         }`}
         onClick={toPrevPage}
         disabled={currentPage === 1}
       >
         <span className="sr-only">Previous</span>
-        <ChevronLeftIcon className="h-5 w-5" aria-hidden="true" />
+        <ChevronLeftIcon className="h-12 w-12" aria-hidden="true" />
       </button>
       {/* Search Bar */}
       <form className="my-auto">
-        <div className="relative">
+        <div className="relative flex items-center">
           <input
-            className="focus:ring-slate-500 h-12 md:h-14 w-48 md:w-80 rounded-lg bg-slate-400 border-gray-200 focus:border-slate-500 focus:outline-slate-500 focus:ring-1 pr-8 pl-3 text-xl placeholder-black focus:z-10"
-            placeholder="Search"
+            className="h-12 md:h-14 w-48 text-3xl placeholder-black focus:outline-none text-center pr-10 placeholder-shown:text-center focus:placeholder-opacity-0"
+            placeholder={isFocused ? "" : "Search"}
             type="text"
             onChange={handleSearchChange}
+            onFocus={() => setIsFocused(true)}
+            onBlur={() => setIsFocused(false)}
           />
-
+          {/* Search Icon */}
           <button
             type="submit"
-            className="absolute inset-y-0 right-0 mr-px rounded-r-lg p-2 text-black"
+            className="absolute inset-y-0 right-0 flex items-center pr-3"
           >
             <span className="sr-only">Submit Search</span>
-            <svg
-              className="h-5 w-5"
-              fill="currentColor"
-              viewBox="0 0 20 20"
-              xmlns="http://www.w3.org/2000/svg"
-            >
-              <path
-                clipRule="evenodd"
-                d="M8 4a4 4 0 100 8 4 4 0 000-8zM2 8a6 6 0 1110.89 3.476l4.817 4.817a1 1 0 01-1.414 1.414l-4.816-4.816A6 6 0 012 8z"
-                fillRule="evenodd"
-              ></path>
-            </svg>
+            <img src={magnify} alt="Search" className="w-8 h-8" />
           </button>
         </div>
       </form>
       {/* Right Button */}
       <button
         type="button"
-        className="rounded-2xl border border-gray-400 bg-slate-400 px-4 py-4  mx-5 text-sm font-medium text-gray-900 hover:bg-slate-300 focus:z-10 focus:border-slate-500 focus:outline-none focus:ring-1 focus:ring-slate-500"
+        className="px-4 py-4 mx-5 md:mx-20"
         onClick={toNextPage}
       >
         <span className="sr-only">Next</span>
-        <ChevronRightIcon className="h-5 w-5" aria-hidden="true" />
+        <ChevronRightIcon className="h-12 w-12" aria-hidden="true" />
       </button>
     </div>
   );
